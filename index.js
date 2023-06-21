b0e6732df748592b5a6cf84c9789401358c9baf7
Paco Garcia <frgarcia@um.es>
2023-06-21 17:05:50 +0200

    uibuild

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var morgan = require('morgan')
 const app = express()
 const Record = require('./models/record')
 
+app.use(express.static('build')) // Servir ficheros estáticos de la carpeta 'build'
 app.use(express.json())
 app.use(cors()) // permitimos solicitudes de todos los orígenes
-app.use(express.static('build')) // Servir ficheros estáticos de la carpeta 'build'
 
 // HTTP request logger middleware for node.js
 morgan.token('body', function (req, res) { 
@@ -27,28 +27,49 @@ app.get('/api/records', (request, response) => {
     )
 })
 
-app.get('/api/records/:id', (request, response) => {
+app.get('/api/records/:id', (request, response, next) => {
     Record.findById(request.params.id).then(record => {
-        response.json(record)
-    }).catch( error => {
-        console.log("Error while retrieving a record...", error)
-        response.status(404).end()
-    })
+        if (record) {
+            
+            response.json(record)
+        } else {
+            console.log("Error while retrieving a record...", request.params.id)    
+            response.status(404).end()
+        }
+    }).catch( error => next(error) )
 })
 
-app.delete('/api/records/:id', (request, response) => {
-    const id = Number(request.params.id)
-    records = records.filter(record => record.id !== id)
-  
-    response.status(204).end()
-})
+app.put('/api/records/:id', (request, response, next) => {
+    const body = request.body
 
-const generateId = () => {
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max)
+    if (!body.fecha || !body.actividad || !body.descripcion || !body.responsable) {
+        return response.status(400).json({ 
+          error: 'missing information' 
+        })
+    } 
+
+    const record = {
+        fecha: body.fecha,
+        duracion: body.duracion,
+        actividad: body.actividad,
+        descripcion: body.descripcion,
+        responsable: body.responsable,
     }
-    return getRandomInt(999999999)
-  }
+
+    Record.findByIdAndUpdate(request.params.id, record, {new: true}) // De forma predeterminada, el parámetro updatedNote del controlador de eventos recibe el documento original sin las modificaciones. Agregamos el parámetro opcional { new: true }, que hará que nuestro controlador de eventos sea llamado con el nuevo documento modificado en lugar del original.
+        .then(updatedRecord => {            
+            response.json(updatedRecord)
+        })
+        .catch( error => next(error) )
+})
+
+app.delete('/api/records/:id', (request, response, next) => {
+    Record.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
 app.post('/api/records', (request, response) => {
     const body = request.body
@@ -65,7 +86,6 @@ app.post('/api/records', (request, response) => {
       actividad: body.actividad,
       descripcion: body.descripcion,
       responsable: body.responsable,
-      id: generateId(),
     })
     
     record.save().then(savedRecord => {
@@ -76,7 +96,20 @@ app.post('/api/records', (request, response) => {
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+// handler of requests with result to errors  
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
