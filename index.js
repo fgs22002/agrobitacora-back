d7ee4a60b4be2fd1b93faeb8140c55422dd91a40
Paco Garcia <frgarcia@um.es>
2023-06-21 15:57:32 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 var morgan = require('morgan')
 const app = express()
+const Record = require('./models/record')
 
 app.use(express.json())
 app.use(cors()) // permitimos solicitudes de todos los orígenes
@@ -14,49 +17,23 @@ morgan.token('body', function (req, res) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let records = [
-    {
-        id: 1, 
-        fecha: "23/05/2023",
-        duracion: 25,
-        actividad: "act1",
-        descripcion: "desc1",
-        responsable: "paco",
-    },
-    {
-        id: 2, 
-        fecha: "26/06/2023",
-        duracion: 50,
-        actividad: "act2",
-        descripcion: "desc2",
-        responsable: "pepe",
-    },
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (request, response) => {
-    const text = "<p>Agrobitacora has info for " + records.length + " records</p>" + "<p>"+ new Date() + "</p>"
-
-    response.send(text)
-})
-
-
 app.get('/api/records', (request, response) => {
-    response.json(records)
+    Record.find({}).then(records => 
+        response.json(records)
+    )
 })
 
 app.get('/api/records/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const record = records.find(record => record.id === id)
-    if (record) {
-       response.json(record)
-    } else {
+    Record.findById(request.params.id).then(record => {
+        response.json(record)
+    }).catch( error => {
+        console.log("Error while retrieving a record...", error)
         response.status(404).end()
-    }
-
+    })
 })
 
 app.delete('/api/records/:id', (request, response) => {
@@ -82,18 +59,18 @@ app.post('/api/records', (request, response) => {
       })
     } 
   
-    const record = {
+    const record = new Record({
       fecha: body.fecha,
       duracion: body.duracion,
       actividad: body.actividad,
       descripcion: body.descripcion,
       responsable: body.responsable,
       id: generateId(),
-    }
-  
-    records = records.concat(record)
-  
-    response.json(record)
+    })
+    
+    record.save().then(savedRecord => {
+        response.json(savedRecord)
+    })    
 })
 
 const unknownEndpoint = (request, response) => {
