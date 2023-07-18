b4ce56d18625bc8e084d71775563242b912d35e4
Paco García <frgarcia@um.es>
2023-07-18 17:00:33 +0200

    corrijo errores tras reestructura

--- models/record.js
+++ models/record.js
@@ -1,31 +1,35 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
 const recordSchema = new mongoose.Schema({
-    fecha: String,
-    duracion: Number,
-    actividad: String,
-    descripcion: String,
-    responsable: String
+  fecha: {
+    type: Date,
+    required: true
+  },
+  duracion: Number,
+  actividad: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  responsable: {
+    type: String,
+    required: true,
+    minlength: 2
+  }
 })
 
 recordSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    returnedObject.fecha = returnedObject.fecha.toISOString().substring(0,10)
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Record', recordSchema)
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,24 +13,24 @@ const url =
 mongoose.connect(url)
 
 const recordSchema = new mongoose.Schema({
-    fecha: String,
-    duracion: Number,
-    actividad: String,
-    descripcion: String,
-    responsable: String
+  fecha: String,
+  duracion: Number,
+  actividad: String,
+  descripcion: String,
+  responsable: String
 })
 
 const Record = mongoose.model('Record', recordSchema)
 
 const record = new Record({
-    fecha: '22/02/2022',
-    duracion: 55,
-    actividad: 'labro',
-    descripcion: 'parcela de abajo',
-    responsable: 'Paco'
+  fecha: '22/02/2022',
+  duracion: 55,
+  actividad: 'labro',
+  descripcion: 'parcela de abajo',
+  responsable: 'Paco'
 })
 
 record.save().then(result => {
-    console.log('record saved!')
-    mongoose.connection.close()
+  console.log('record saved!')
+  mongoose.connection.close()
 })
